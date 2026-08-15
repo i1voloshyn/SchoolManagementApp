5f1f1556113d776b6ef71028d8a157deb6545e80
Ivan Voloshyn <ivan.voloshyn.ph@gmail.com>
2026-08-15 11:35:11 +0200

    refactor: remove unnecessary DataInitialisationRunner

--- src/main/java/com/foxminded/schoolmanagementapp/application/WelcomeApp.java
+++ src/main/java/com/foxminded/schoolmanagementapp/application/WelcomeApp.java
@@ -3,25 +3,25 @@ package com.foxminded.schoolmanagementapp.application;
 import com.foxminded.schoolmanagementapp.repository.GreetingOutput;
 import com.foxminded.schoolmanagementapp.repository.GreetingProvider;
 import com.foxminded.schoolmanagementapp.repository.GroupRepository;
+import com.foxminded.schoolmanagementapp.util.datagenerator.DataGenerator;
+import lombok.AllArgsConstructor;
 import org.jspecify.annotations.NonNull;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+@AllArgsConstructor
 @Component
-@Order(2)
 public class WelcomeApp implements ApplicationRunner {
     private final GreetingOutput greetingOutput;
     private final GreetingProvider greetingProvider;
-
-    public WelcomeApp(GreetingOutput greetingOutput, GreetingProvider greetingProvider) {
-        this.greetingOutput = greetingOutput;
-        this.greetingProvider = greetingProvider;
-    }
+    private final DataGenerator dataGenerator;
 
     @Override
     public void run(@NonNull ApplicationArguments args) {
+        dataGenerator.generateDataIfEmpty();
+
         String greeting = greetingProvider.getGreeting();
         greetingOutput.print(greeting);
     }
